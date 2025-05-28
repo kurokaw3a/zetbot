9282e8a5473ac0502c861c5d083afcc77a91b20f
kurokaw3a <kamaldinov321@gmail.com>
2025-05-28 18:44:33 +0600

    master

--- constants.py
+++ constants.py
@@ -1,9 +1,10 @@
 import database
 data = database.get_bot_data()
+props = database.get_props()
 
 bot_admin = data["admin"] or "none"
 bot_props = data["props"] or "none"
-bot_new_props = ""
+bot_new_props = data["props"] or "none"
 
 channel = "-1002346029421"
 channel_link = "https://t.me/zetkassakg"
